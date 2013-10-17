8872c1aa9fb6583b2ef8289511f50766b9e775b6
cafama <matteo.cafasso@f-secure.com>
2013-10-17 16:19:36 +0300

    bump version to 0.1.2

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read(fname):
 
 setup(
     name="Pebble",
-    version="0.1.0",
+    version="0.1.2",
     author="Matteo Cafasso",
     author_email="noxdafox@gmail.com",
     description=("Threading and multiprocessing eye-candy."),

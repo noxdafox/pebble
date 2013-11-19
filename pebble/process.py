c388ab5423b1a72e47d222b13400474e9a1caa54
cafama <matteo.cafasso@f-secure.com>
2013-11-19 16:15:35 +0200

    move concurrent decorator to process module

---
+++ pebble/process.py
@@ -0,0 +1,158 @@
+# This file is part of Pebble.
+
+# Pebble is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License
+# as published by the Free Software Foundation,
+# either version 3 of the License, or (at your option) any later version.
+
+# Pebble is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+
+# You should have received a copy of the GNU Lesser General Public License
+# along with Pebble.  If not, see <http://www.gnu.org/licenses/>.
+
+
+from uuid import uuid4
+from traceback import format_exc
+from itertools import count
+from threading import Thread
+from collections import Callable
+from functools import update_wrapper
+from multiprocessing import Process, Pipe, TimeoutError
+try:  # Python 2
+    from cPickle import PicklingError
+except:  # Python 3
+    from pickle import PicklingError
+
+from .pebble import PebbleError
+
+
+def worker(function, writer, *args, **kwargs):
+    try:
+        writer.send(function(*args, **kwargs))
+    except (IOError, OSError):  # pipe was closed
+        return
+    except Exception as error:
+        error.traceback = format_exc()
+        try:
+            writer.send(error)
+        except PicklingError:
+            writer.send(SerializingError(str(error), type(error)))
+
+
+def concurrent(*args, **kwargs):
+    """Turns a *function* into a Process and runs its logic within.
+
+    A decorated *function* will return a *Task* object once is called.
+
+    If *callback* is a callable, it will be called once the task has ended
+    with the task identifier and the *function* return values.
+
+    """
+    def wrapper(function):
+        return Wrapper(function, timeout, callback)
+
+    if len(args) == 1 and not len(kwargs) and isinstance(args[0], Callable):
+        return Wrapper(args[0], None, None)
+    elif not len(args) and len(kwargs):
+        timeout = kwargs.get('timeout', None)
+        callback = kwargs.get('callback', None)
+
+        return wrapper
+    else:
+        raise ValueError("Decorator accepts only keyword arguments.")
+
+
+class SerializingError(PebbleError):
+    """Raised if unable to serialize an Exception."""
+    def __init__(self, msg, value):
+        super(SerializingError, self).__init__(msg, value)
+        self.msg = msg
+        self.value = value
+
+    def __repr__(self):
+        return "%s %s: %s\n%s" % (self.__class__, self.value, self.msg)
+
+    def __str__(self):
+        return "Unable to serialize %s. Message: %s" % (self.value, self.msg)
+
+
+class Task(object):
+    def __init__(self, task_nr, worker, reader, callback, timeout):
+        self.id = uuid4()
+        self.number = task_nr
+        self._cancelled = False
+        self._timeout = timeout
+        self._results = None
+        self._reader = reader
+        self._worker = worker
+        self._callback = callback
+        self._worker_listener = Thread(target=self._set)
+        self._worker_listener.daemon = True
+        self._worker_listener.start()
+
+    @property
+    def cancelled(self):
+        return self._cancelled
+
+    def get(self, timeout=None):
+        """Retrieves the produced results.
+
+        If the executed code raised an error it will be re-raised.
+
+        """
+        self._worker_listener.join(timeout)
+        if not self._worker_listener.is_alive():
+            if (isinstance(self._results, BaseException)):
+                raise self._results
+            else:
+                return self._results
+        else:
+            raise TimeoutError("Task is still running")
+
+    def ready(self):
+        """Returns True if results are ready."""
+        return not self._worker_listener.is_alive()
+
+    def cancel(self):
+        """Cancels the Task terminating the running process
+        and dropping the results."""
+        self._cancelled = True
+        self._worker.terminate()
+        self._reader.close()
+
+    def _set(self):
+        try:
+            if self._reader.poll(self._timeout):
+                self._results = self._reader.recv()
+                if self._callback is not None:
+                    self._callback(self)
+            elif self._worker.is_alive():
+                self._worker.terminate()
+        except (IOError, OSError) as error:  # pipe was closed
+            if not self._cancelled:
+                self._results = error
+        finally:
+            self._worker.join()
+
+
+class Wrapper(object):
+    def __init__(self, function, timeout, callback):
+        self._function = function
+        self._counter = count()
+        self.timeout = timeout
+        self.callback = callback
+        update_wrapper(self, function)
+
+    def __call__(self, *args, **kwargs):
+        reader, writer = Pipe(duplex=False)
+        args = list(args)
+        args.insert(0, self._function)
+        args.insert(1, writer)
+        p = Process(target=worker, args=(args), kwargs=(kwargs))
+        p.daemon = True
+        p.start()
+        return Task(next(self._counter), p, reader,
+                    self.callback, self.timeout)

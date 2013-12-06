4567e1520eb3c6951e3543aeef96b8eb5c9d7218
noxdafox <noxdafox@gmail.com>
2013-12-06 16:36:01 +0200

    release 2.0.2

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read(fname):
 
 setup(
     name="Pebble",
-    version="2.0.1",
+    version="2.0.2",
     author="Matteo Cafasso",
     author_email="noxdafox@gmail.com",
     description=("Threading and multiprocessing eye-candy."),

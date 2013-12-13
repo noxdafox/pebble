ff0a71095771707daeba1289bbd53217952aa225
Alan Justino <alan.justino@yahoo.com.br>
2013-12-13 13:58:56 -0200

    Added @synchronized(lock) decorator

---
+++ pebble/synchronization.py
@@ -0,0 +1,31 @@
+# This file is part of Pebble.
+
+# Pebble is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License
+# as published by the Free Software Foundation,
+# either version 3 of the License, or (at your option) any later version.
+
+# Pebble is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+
+# You should have received a copy of the GNU Lesser General Public License
+# along with Pebble.  If not, see <http://www.gnu.org/licenses/>.
+
+
+def synchronized(lock):
+    """ Synchronization decorator. Locks the execution on given 'lock'
+
+    Works with both threading and multiprocessing Lock s
+    """
+
+    def wrap(f):
+        def new_function(*args, **kw):
+            lock.acquire()
+            try:
+                return f(*args, **kw)
+            finally:
+                lock.release()
+        return new_function
+    return wrap

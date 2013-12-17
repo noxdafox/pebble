287398b4d9449cb3e51a610fb6487df6d3f5355d
cafama <matteo.cafasso@f-secure.com>
2013-12-17 16:43:40 +0200

    release 2.1.1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read(fname):
 
 setup(
     name="Pebble",
-    version="2.1.0",
+    version="2.1.1",
     author="Matteo Cafasso",
     author_email="noxdafox@gmail.com",
     description=("Threading and multiprocessing eye-candy."),

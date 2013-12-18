11a363dee9f2a640e44b63d2313f8dd7cb2cbd10
cafama <matteo.cafasso@f-secure.com>
2013-12-18 14:18:18 +0200

    repare process for pool

--- pebble/__init__.py
+++ pebble/__init__.py
@@ -3,7 +3,6 @@ __all__ = ['thread',
            'process',
            'synchronized',
            'PebbleError',
-           'SerializingError',
            'TimeoutError',
            'TaskCancelled']
 

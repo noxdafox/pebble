23ad8ddaccd29327369c4dd2dfd59dac6dbd9bf9
noxdafox <noxdafox@gmail.com>
2014-04-13 20:20:05 +0300

    release 2.4.7: refactored Pools logic, improved performance, improved overall stability, fixed some bug

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read(fname):
 
 setup(
     name="Pebble",
-    version="2.4.6",
+    version="2.4.7",
     author="Matteo Cafasso",
     author_email="noxdafox@gmail.com",
     description=("Threading and multiprocessing eye-candy."),

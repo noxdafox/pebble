680456e6c43a1f9d05f2c733bcea246134d3c34d
noxdafox <noxdafox@gmail.com>
2014-04-18 01:01:24 +0300

    release 2.4.8: fix process leak due to bug in process decorator

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read(fname):
 
 setup(
     name="Pebble",
-    version="2.4.7",
+    version="2.4.8",
     author="Matteo Cafasso",
     author_email="noxdafox@gmail.com",
     description=("Threading and multiprocessing eye-candy."),

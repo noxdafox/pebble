8433aca41246f977d2cae6ba9d18d22ac46191b2
cafama <matteo.cafasso@f-secure.com>
2014-04-29 16:10:56 +0300

    release 2.5.0: decorators can now decorate instance methods correctly

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read(fname):
 
 setup(
     name="Pebble",
-    version="2.4.9",
+    version="2.5.0",
     author="Matteo Cafasso",
     author_email="noxdafox@gmail.com",
     description=("Threading and multiprocessing eye-candy."),

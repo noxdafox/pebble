c217e53ebe1b77fcdd549d16682bd6c4837cad64
noxdafox <noxdafox@gmail.com>
2014-05-30 00:32:55 +0300

    remove deprecated pools from package

--- pebble/__init__.py
+++ pebble/__init__.py
@@ -1,13 +1,9 @@
 __all__ = ['synchronized',
            'sighandler',
            'Task',
-           'ThreadPool',
            'PebbleError',
-           'ProcessPool',
            'TimeoutError',
            'TaskCancelled']
 
 from .pebble import PebbleError, TaskCancelled, TimeoutError, Task
 from .pebble import synchronized, sighandler
-from .pools.process import ProcessPool
-from .pools.thread import ThreadPool

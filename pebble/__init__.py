36297ec13d34d93804df7a10aa794897aecd4dd2
cafama <matteo.cafasso@f-secure.com>
2014-05-30 15:39:01 +0300

    fix main package import

--- pebble/__init__.py
+++ pebble/__init__.py
@@ -1,9 +1,14 @@
-__all__ = ['synchronized',
+__all__ = ['process',
+           'thread',
+           'synchronized',
            'sighandler',
            'Task',
            'PebbleError',
            'TimeoutError',
            'TaskCancelled']
 
-from .pebble import PebbleError, TaskCancelled, TimeoutError, Task
+
+import thread
+import process
 from .pebble import synchronized, sighandler
+from .pebble import PebbleError, TaskCancelled, TimeoutError, Task

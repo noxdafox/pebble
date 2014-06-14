8f879c10090db81805aa3391573fa2f0aca07183
noxdafox <noxdafox@gmail.com>
2014-06-14 13:34:41 +0300

    release 3.1.2: manage termination error in Python 3.3

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read(fname):
 
 setup(
     name="Pebble",
-    version="3.1.1",
+    version="3.1.2",
     author="Matteo Cafasso",
     author_email="noxdafox@gmail.com",
     description=("Threading and multiprocessing eye-candy."),

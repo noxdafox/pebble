9fd6c33a9d5d6a0e87b3ea567edf61fed40ad3c9
noxdafox <noxdafox@gmail.com>
2014-06-14 13:25:40 +0300

    release 3.1.1: fix waitforthreads to support Python >= 3.4

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read(fname):
 
 setup(
     name="Pebble",
-    version="3.1.0",
+    version="3.1.1",
     author="Matteo Cafasso",
     author_email="noxdafox@gmail.com",
     description=("Threading and multiprocessing eye-candy."),

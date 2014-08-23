e2c3fd445a44bac33ee1f3bd537679d5090a7992
noxdafox <noxdafox@gmail.com>
2014-08-23 23:59:05 +0300

    first draft

--- pebble/process/pool.py
+++ pebble/process/pool.py
@@ -16,6 +16,7 @@
 
 import os
 import sys
+import asyncio
 
 from itertools import count
 from time import sleep, time
@@ -36,8 +37,267 @@ from ..pebble import STOPPED, RUNNING, ERROR
 from ..pebble import Task, TimeoutError, TaskCancelled
 
 
-ACK = 0  # task aknowledged by worker
-RES = 1  # task results from worker
+@spawn_process(name='pool_worker', daemon=True)
+def pool_worker(tasks, results, initializer, initargs, limit):
+    """Runs the actual function in separate process."""
+    error = None
+    value = None
+    counter = count()
+    pid = os.getpid()
+    signal(SIGINT, SIG_IGN)
+
+    if initializer is not None:
+        try:
+            initializer(*initargs)
+        except Exception as err:
+            error = err
+            error.traceback = format_exc()
+
+    while limit == 0 or next(counter) < limit:
+        number, function, args, kwargs = get_task(channel, pid)
+
+        try:
+            value = function(*args, **kwargs)
+        except Exception as err:
+            if error is None:
+                error = err
+                error.traceback = format_exc()
+
+        try:
+            channel.send((RES, number, error is not None and error or value))
+        except PicklingError as error:
+            channel.send((RES, number, error))
+
+        error = None
+        value = None
+
+    sys.exit(0)
+
+
+@asyncio.coroutine
+def worker_manager(context, loop):
+    """Collects expired workers and spawns new ones."""
+    pool = context.pool
+    limit = context.worker_limit
+    initializer = context.initializer
+    initargs = context.initargs
+    workers = context.worker_number
+
+    if context.state not in (ERROR, STOPPED):
+        expired = [w for w in pool if not w.is_alive()]
+
+        for worker in expired:
+            worker.join()
+            del pool[worker.pid]
+
+        for _ in range(workers - len(pool)):
+            worker = Worker(initializer, initargs, limit)
+            pool.appen(worker)
+
+        loop.call_later(0.2, worker_manager, context, loop)
+
+
+@asyncio.coroutine
+def task_scheduler(context):
+    """Schedules enqueued tasks to the workers."""
+    queue = context.queue
+
+    while context.state not in (ERROR, STOPPED):
+        workers = [w for w in pool if not w.closed]
+
+        for worker in workers:
+            try:
+                task = yield from queue.get()
+
+                worker.schedule(task)
+            except Empty:
+                continue
+
+
+@asyncio.coroutine
+def result_manager(context):
+    """."""
+    pending = []
+    done = context.task_done
+
+    while context.state not in (ERROR, STOPPED):
+        workers = [w for w in pool if not w.expired]
+
+        for worker in workers:
+            future = asyncio.Future()
+            asyncio.async(worker.result(future))
+            pending.append(future)
+
+        done, pending = yield from asyncio.wait(
+            pending, timeout=0.2, return_when=asyncio.FIRST_COMPLETED)
+
+        for future in done:
+            task, result = future.result()
+            task._set(result)
+            done(task)
+
+
+@spawn_thread(name='pool_manager', daemon=True)
+def pool_manager(context, loop):
+    """Schedules enqueued tasks to the workers."""
+    loop.call_soon(workers_manager(context))
+    loop.call_soon(task_scheduler(context))
+    loop.call_soon(result_manager(context))
+    loop.call_soon(task_manager(context))
+
+    loop.run_forever()
+
+
+class Worker(object):
+    def __init__(self, initializer, initargs, limit)
+    self.initializer = initializer
+    self.initargs = initargs
+    self.limit = limit
+    self.counter = count()
+    self.tasks = deque()
+
+    @property
+    def closed(self):
+        return self.task_writer.closed
+
+    @property
+    def expired(self):
+        return self.result_reader.closed
+
+    def start(self):
+        self.task_reader, self.task_writer = pipe()
+        self.result_reader, self.result_writer = pipe()
+        self.process = pool_worker(self.task_reader, self.result_writer,
+                                   self.initializer, self.initargs, self.limit)
+        self.task_reader.close()
+        self.result_writer.close()
+
+    def schedule(self, task):
+        if not self.tasks:
+            task._timestamp = time()
+        self.tasks.append(task)
+
+        self.task_writer.send(task)
+
+        if next(self.counter) == limit:
+            self.task_writer.close()
+
+    @asyncio.coroutine
+    def result(self, future):
+        try:
+            data = yield from self.result_reader.recv()
+            task = self.tasks.popleft()
+            future.set_result((task, data))
+        except EOFError, IOError as error:
+            self.result_reader.close()
+            raise
+
+
+class PoolTask(Task):
+    """Extends the *Task* object to support *process* decorator."""
+    def _cancel(self):
+        """Overrides the *Task* cancel method."""
+        self._cancelled = True
+
+
+class Context(PoolContext):
+    """Pool's Context."""
+    def __init__(self, queue, queueargs, initializer, initargs,
+                 workers, limit):
+        super(Context, self).__init__(queue, queueargs,
+                                      initializer, initargs,
+                                      workers, limit)
+
+    def stop(self):
+        """Stop the workers."""
+        with lock(self.worker_channel):
+            for worker in self.pool.values():
+                worker.terminate()
+
+    def kill(self):
+        """Forces all workers to stop."""
+        self.stop()
+
+        with lock(self.worker_channel):
+            for worker in self.pool.values():
+                stop_worker(worker)
+
+
+class Pool(BasePool):
+    """A ProcessPool allows to schedule jobs into a Pool of Processes
+    which will perform them concurrently.
+
+    workers is an integer representing the amount of desired process workers
+    managed by the pool.
+    If worker_task_limit is a number greater than zero,
+    each worker will be restarted after performing an equal amount of tasks.
+    initializer must be callable, if passed, it will be called
+    every time a worker is started, receiving initargs as arguments.
+    queue represents a Class which, if passed, will be constructed
+    with queueargs as parameters and used internally as a task queue.
+    The queue object resulting from its construction must expose
+    same functionalities of Python standard Queue object,
+    especially for what concerns the put(), get() and join() methods.
+
+    """
+    def __init__(self, workers=1, task_limit=0, queue=None, queueargs=None,
+                 initializer=None, initargs=()):
+        super(Pool, self).__init__()
+        self._context = Context(queue, queueargs, initializer, initargs,
+                                workers, task_limit)
+
+    def _start(self):
+        """Start the Pool managers."""
+        self._managers = (task_scheduler(self._context),
+                          task_manager(self._context),
+                          message_manager(self._context),
+                          worker_manager(self._context))
+        self._context.state = RUNNING
+
+    def stop(self):
+        """Stops the pool without performing any pending task."""
+        self._context.state = STOPPED
+
+        self._context.queue.put(None)  # task_scheduler stop sentinel
+        self._context.worker_channel.send(None)  # task_manager stop sentinel
+
+        for manager in self._managers:
+            manager.join()
+
+        self._context.stop()
+
+    def kill(self):
+        """Kills the pool forcing all workers to terminate immediately."""
+        self._context.state = STOPPED
+
+        self._context.kill()
+
+    def schedule(self, function, args=(), kwargs={}, identifier=None,
+                 callback=None, timeout=0):
+        """Schedules *function* into the Pool, passing *args* and *kwargs*
+        respectively as arguments and keyword arguments.
+
+        If *callback* is a callable it will be executed once the function
+        execution has completed with the returned *Task* as a parameter.
+
+        *timeout* is an integer, if expires the task will be terminated
+        and *Task.get()* will raise *TimeoutError*.
+
+        The *identifier* value will be forwarded to the *Task.id* attribute.
+
+        A *Task* object is returned.
+
+        """
+        task = PoolTask(next(self._counter), function, args, kwargs,
+                        callback, timeout, identifier)
+
+        self._schedule(task)
+
+        return task
+
+
+
+
 
 
 def get_task(channel, pid):
@@ -87,43 +347,6 @@ def task_state(task, pool, timestamp):
             error = True
 
     return error, results
-
-
-@spawn_process(name='pool_worker', daemon=True)
-def pool_worker(channel, initializer, initargs, limit):
-    """Runs the actual function in separate process."""
-    error = None
-    value = None
-    counter = count()
-    pid = os.getpid()
-    signal(SIGINT, SIG_IGN)
-
-    if initializer is not None:
-        try:
-            initializer(*initargs)
-        except Exception as err:
-            error = err
-            error.traceback = format_exc()
-
-    while limit == 0 or next(counter) < limit:
-        number, function, args, kwargs = get_task(channel, pid)
-
-        try:
-            value = function(*args, **kwargs)
-        except Exception as err:
-            if error is None:
-                error = err
-                error.traceback = format_exc()
-
-        try:
-            channel.send((RES, number, error is not None and error or value))
-        except PicklingError as error:
-            channel.send((RES, number, error))
-
-        error = None
-        value = None
-
-    sys.exit(0)
 
 
 @spawn_thread(name='task_scheduler', daemon=True)
@@ -237,107 +460,3 @@ def worker_manager(context):
             pool[worker.pid] = worker
 
         sleep(0.2)
-
-
-class PoolTask(Task):
-    """Extends the *Task* object to support *process* decorator."""
-    def _cancel(self):
-        """Overrides the *Task* cancel method."""
-        self._cancelled = True
-
-
-class Context(PoolContext):
-    """Pool's Context."""
-    def __init__(self, queue, queueargs, initializer, initargs,
-                 workers, limit):
-        super(Context, self).__init__(queue, queueargs,
-                                      initializer, initargs,
-                                      workers, limit)
-        self.pool_channel, self.worker_channel = channels()
-
-    def stop(self):
-        """Stop the workers."""
-        with lock(self.worker_channel):
-            for worker in self.pool.values():
-                worker.terminate()
-
-    def kill(self):
-        """Forces all workers to stop."""
-        self.stop()
-
-        with lock(self.worker_channel):
-            for worker in self.pool.values():
-                stop_worker(worker)
-
-
-class Pool(BasePool):
-    """A ProcessPool allows to schedule jobs into a Pool of Processes
-    which will perform them concurrently.
-
-    workers is an integer representing the amount of desired process workers
-    managed by the pool.
-    If worker_task_limit is a number greater than zero,
-    each worker will be restarted after performing an equal amount of tasks.
-    initializer must be callable, if passed, it will be called
-    every time a worker is started, receiving initargs as arguments.
-    queue represents a Class which, if passed, will be constructed
-    with queueargs as parameters and used internally as a task queue.
-    The queue object resulting from its construction must expose
-    same functionalities of Python standard Queue object,
-    especially for what concerns the put(), get() and join() methods.
-
-    """
-    def __init__(self, workers=1, task_limit=0, queue=None, queueargs=None,
-                 initializer=None, initargs=()):
-        super(Pool, self).__init__()
-        self._context = Context(queue, queueargs, initializer, initargs,
-                                workers, task_limit)
-
-    def _start(self):
-        """Start the Pool managers."""
-        self._managers = (task_scheduler(self._context),
-                          task_manager(self._context),
-                          message_manager(self._context),
-                          worker_manager(self._context))
-        self._context.state = RUNNING
-
-    def stop(self):
-        """Stops the pool without performing any pending task."""
-        self._context.state = STOPPED
-
-        self._context.queue.put(None)  # task_scheduler stop sentinel
-        self._context.worker_channel.send(None)  # task_manager stop sentinel
-
-        for manager in self._managers:
-            manager.join()
-
-        self._context.stop()
-
-    def kill(self):
-        """Kills the pool forcing all workers to terminate immediately."""
-        self._context.state = STOPPED
-
-        self._context.kill()
-
-    def schedule(self, function, args=(), kwargs={}, identifier=None,
-                 callback=None, timeout=0):
-        """Schedules *function* into the Pool, passing *args* and *kwargs*
-        respectively as arguments and keyword arguments.
-
-        If *callback* is a callable it will be executed once the function
-        execution has completed with the returned *Task* as a parameter.
-
-        *timeout* is an integer, if expires the task will be terminated
-        and *Task.get()* will raise *TimeoutError*.
-
-        The *identifier* value will be forwarded to the *Task.id* attribute.
-
-        A *Task* object is returned.
-
-        """
-        task = PoolTask(next(self._counter), function, args, kwargs,
-                        callback, timeout, identifier)
-
-        self._schedule(task)
-
-        return task

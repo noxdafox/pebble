08ca0273d17880a1a0306c40a739ae7108dac610
noxdafox <noxdafox@gmail.com>
2014-08-27 00:13:02 +0300

    release 3.1.7: bugfixes

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read(fname):
 
 setup(
     name="Pebble",
-    version="3.1.6",
+    version="3.1.7",
     author="Matteo Cafasso",
     author_email="noxdafox@gmail.com",
     description=("Threading and multiprocessing eye-candy."),

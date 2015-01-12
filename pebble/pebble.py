d81c6b2e2fe2b911ae3ecb644f7f1a0fdaea8846
cafama <matteo.cafasso@f-secure.com>
2015-01-12 15:09:40 +0200

    added comment

--- pebble/pebble.py
+++ pebble/pebble.py
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with Pebble.  If not, see <http://www.gnu.org/licenses/>.
 
-"""Container for generic objects."""
+# Pebble's generic objects
 
 
 import signal

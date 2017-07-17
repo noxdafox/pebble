1d4085aa967eae37aeef403663bdc4e45bf93660
Matteo Cafasso <noxdafox@gmail.com>
2017-07-17 22:02:50 +0300

    lol
    Signed-off-by: Matteo Cafasso <noxdafox@gmail.com>

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
-    - python setup.py install
+    python setup.py install
 else
-    - sudo apt-get update -qq
-    - sudo apt-get install -qq python-setuptools python3-setuptools
-    - python setup.py install
+    sudo apt-get update -qq
+    sudo apt-get install -qq python-setuptools python3-setuptools
+    python setup.py install
 fi

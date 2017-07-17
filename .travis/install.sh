44b9a446b42dedb54ae17022303c8299efb6fa97
Matteo Cafasso <noxdafox@gmail.com>
2017-07-17 21:56:32 +0300

    osx tests
    Signed-off-by: Matteo Cafasso <noxdafox@gmail.com>

---
+++ .travis/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+    - python setup.py install
+else
+    - sudo apt-get update -qq
+    - sudo apt-get install -qq python-setuptools python3-setuptools
+    - python setup.py install
+fi

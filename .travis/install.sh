988c6605e4cd722124df14c5cb6afd7f112b2184
Matteo Cafasso <noxdafox@gmail.com>
2017-07-18 22:40:22 +0300

    asdas
    Signed-off-by: Matteo Cafasso <noxdafox@gmail.com>

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+    brew update
+    brew install python3
+    virtualenv venv -p python3
+    source venv/bin/activate
     python setup.py install
 else
-    pip install -U pip
-    pip install -U setuptools
+    sudo pip install --upgrade pip
+    sudo pip install --upgrade setuptools
     python setup.py install
 fi

e7211b5f00b2090a2c6451503ba455c67f671e53
Matteo Cafasso <noxdafox@gmail.com>
2017-11-06 21:43:50 +0200

    release 4.3.4
    Signed-off-by: Matteo Cafasso <noxdafox@gmail.com>

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setup(
     name="Pebble",
-    version="4.3.3",
+    version="4.3.4",
     author="Matteo Cafasso",
     author_email="noxdafox@gmail.com",
     description=("Threading and multiprocessing eye-candy."),

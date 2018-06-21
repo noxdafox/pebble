815daf7c91c09f63a273ff0b703c77f0c08c9473
Matteo Cafasso <noxdafox@gmail.com>
2018-06-22 00:59:22 +0300

    Add copyright years
    Signed-off-by: Matteo Cafasso <noxdafox@gmail.com>

--- pebble/common.py
+++ pebble/common.py
@@ -1,3 +1,19 @@
+# This file is part of Pebble.
+# Copyright (c) 2013-2018, Matteo Cafasso
+
+# Pebble is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License
+# as published by the Free Software Foundation,
+# either version 3 of the License, or (at your option) any later version.
+
+# Pebble is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+
+# You should have received a copy of the GNU Lesser General Public License
+# along with Pebble.  If not, see <http://www.gnu.org/licenses/>.
+
 from __future__ import absolute_import
 
 import os

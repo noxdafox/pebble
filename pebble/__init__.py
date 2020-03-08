161b900419cdf8cc1407b584a05f8f5e2e576787
Matteo Cafasso <noxdafox@gmail.com>
2020-03-08 15:58:21 +0200

    Update version files
    Signed-off-by: Matteo Cafasso <noxdafox@gmail.com>

--- pebble/__init__.py
+++ pebble/__init__.py
@@ -14,3 +14,6 @@ from pebble.decorators import synchronized, sighandler
 from pebble.common import ProcessExpired, ProcessFuture
 from pebble.functions import waitforqueues, waitforthreads
 from pebble.pool import ThreadPool, ProcessPool, MapFuture, ProcessMapFuture
+"""Versioning controlled via Git Tag, check setup.py"""
+
+__version__ = "4.5.1"
--- version.py
+++ version.py
@@ -1,3 +1,3 @@
 """Versioning controlled via Git Tag, check setup.py"""
 
-__version__ = "4.5.0"
+__version__ = "4.5.1"

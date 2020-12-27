6f0358aeb6711a8766b8f936687e61579641261c
Matteo Cafasso <noxdafox@gmail.com>
2020-12-27 12:44:45 +0200

    Update Copyright years
    Signed-off-by: Matteo Cafasso <noxdafox@gmail.com>

--- pebble/pool/base_pool.py
+++ pebble/pool/base_pool.py
@@ -1,5 +1,5 @@
 # This file is part of Pebble.
-# Copyright (c) 2013-2020, Matteo Cafasso
+# Copyright (c) 2013-2021, Matteo Cafasso
 
 # Pebble is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License

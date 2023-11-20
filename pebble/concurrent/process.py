0da99703e9885c43a2ae4b98068268fc37cede9d
Matteo Cafasso <noxdafox@gmail.com>
2023-11-20 22:19:10 +0200

    Update copyright year
    Signed-off-by: Matteo Cafasso <noxdafox@gmail.com>

--- pebble/concurrent/process.py
+++ pebble/concurrent/process.py
@@ -1,5 +1,5 @@
 # This file is part of Pebble.
-# Copyright (c) 2013-2022, Matteo Cafasso
+# Copyright (c) 2013-2023, Matteo Cafasso
 
 # Pebble is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License
